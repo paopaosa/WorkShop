146245dcb9c23342b02d0963d1cebb62d752f113
paopaosa <paopaosa@gmail.com>
2020-12-10 13:33:19 +0800

    init

---
+++ Shared/WorkShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkShopApp.swift
+//  Shared
+//
+//  Created by Paopao Sa on 2020/12/10.
+//
+
+import SwiftUI
+
+@main
+struct WorkShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
